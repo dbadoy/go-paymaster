48d9a7787a7a3036332b972d6f9e4d908e6f0c12
Seungbae Yu <dbadoy4874@gmail.com>
2024-01-27 10:06:30 +0900

    pkg/bundler: renamed

--- pkg/bundler/bundler.go
+++ pkg/bundler/bundler.go
@@ -8,18 +8,18 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-type BundlerClient struct {
+type Client struct {
 	entryPoint common.Address
 	c          *rpc.Client
 }
 
-func (b *BundlerClient) SendUserOperation(ctx context.Context, op *userop.UserOperation) (common.Hash, error) {
+func (b *Client) SendUserOperation(ctx context.Context, op *userop.UserOperation) (common.Hash, error) {
 	var result common.Hash
 	err := b.c.CallContext(ctx, &result, "eth_sendUserOperation", op, b.entryPoint)
 	return result, err
 }
 
-func (b *BundlerClient) EstimateUserOperationGas(ctx context.Context, op *userop.UserOperation) (*GasEstimates, error) {
+func (b *Client) EstimateUserOperationGas(ctx context.Context, op *userop.UserOperation) (*GasEstimates, error) {
 	var estimate GasEstimates
 	err := b.c.CallContext(ctx, &estimate, "eth_estimateUserOperationGas", op, b.entryPoint)
 	if err != nil {
